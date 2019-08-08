ab2083e93bee1c2a6ae7d02cd8e4e931ad68a58d
papadave66 <papadave@foxmail.com>
2019-08-08 09:13:49 +0800

    support markdown now

--- django_blog/views.py
+++ django_blog/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -14,4 +15,12 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    #post.body = markdown.markdown(post.body, extensions=['extra', 'codehilite', 'toc', ])
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'detail.html', context={'post': post})
